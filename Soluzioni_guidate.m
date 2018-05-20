626121ed692d64840140e835b80a7651eca7ddfa
giusdp <depalma.gsp@gmail.com>
2018-05-20 20:24:51 +0200

    finite le soluzione guidate

--- Soluzioni_guidate.m
+++ Soluzioni_guidate.m
@@ -2,20 +2,55 @@
 
 Sol1 = PopupWindow[
     Button[Style["Vuoi vedere la soluzione?", FontSize->Large]],
-    TabView[{Text[Style[" Trucco >>> ", 36, Orange, Bold]] -> Text[Style["\n Seguire i passi per vedere la soluzione passo dopo passo...\n\n", 44]],
-    Text[Style["Primo Passo", 36, Orange, Bold]] -> Text["Dobbiamo quindi risolvere:\n\n" Framed[MaTeX["\\int f(x) \,dx=\\int\\left[ 3e^x+2x^2-\\frac{1}{2}\\cos x \\right] \, dx", Magnification->3], BaseStyle->{Orange}], BaseStyle->{FontSize->32}],
-    Text[Style["Secondo Passo", 36, Orange, Bold]] -> Text["Usiamo la II proprieta' di linearita' per portare fuori le varie costanti dagli integrali:\n\n" Framed[MaTeX["3\\int_{1}^{e} x^2 \, dx-2\\int_{1}^{e}\\frac{1}{x} \, dx", Magnification->3],BaseStyle->{Orange}], BaseStyle->{FontSize->32}],
-    Text[Style["Terzo Passo", 36, Orange, Bold]] -> Text["Risolviamo separatamente i due integrali fondamentali e quindi:\n\n" Framed[MaTeX["3\\int_{1}^{e} x^2 \,dx=3((\\frac{1}{3}e^3)-\\frac{1}{3}1^3) \\quad e \\quad -2\\int_{1}^{e} \\frac{1}{x} \,dx= -2\\log (\\frac{e}{1})", Magnification->3], BaseStyle->{Orange}], BaseStyle->{FontSize->32}], 
-    Text[Style["Quarto Passo", 36, Orange, Bold]] -> Text["Rimettendoli insieme avremo: \n\n" Framed[MaTeX["3((\\frac{1}{3}e^3)-\\frac{1}{3}1^3)-2\\log (\\frac{e}{1})=e^3-3", Magnification->3], BaseStyle->{Orange}], BaseStyle->{FontSize->32}]}, Appearance->Large],
-    WindowTitle->"Soluzione Guidata", WindowSize->All
-]
+
+    TabView[
+            {
+                (*Text[Style[" Trucco >>> ", 28, Orange, Bold]] 
+                    -> Text[Style["\n Seguire i passi per vedere la soluzione ->", 24]],*)
+                Text[Style["Primo Passo", 28, Orange, Bold]] 
+                    -> Text["Dobbiamo quindi risolvere:\n" 
+                        Framed[MaTeX["\\int f(x) \,dx=\\int\\left[ 3e^x+2x^2-\\frac{1}{2}\\cos x \\right] \, dx", Magnification->3], 
+                        BaseStyle->{Orange}], BaseStyle->{FontSize->24}],
+                Text[Style["Secondo Passo", 28, Orange, Bold]]
+                    -> Text["Utilizziamo la 1° proprietà di linearità per spezzare l'integrale \nin tre pezzi, facendolo diventare uguale a \n" 
+                        Framed[MaTeX["\\int 3e^x\, dx+\\int 2x^2\, dx-\\int \\frac{1}{2}\\cos x\, dx", Magnification->3],BaseStyle->{Orange}], 
+                        BaseStyle->{FontSize->24}],
+                Text[Style["Terzo Passo", 28, Orange, Bold]] 
+                    -> Text["Utilizziamo poi la 2° proprietà di linearità per portar fuori le varie \ncostanti dai tre integrali, quindi avremo\n"
+                        Framed[MaTeX["3\\int e^x\, dx+2\\int x^2\, dx-\\frac{1}{2}\\int \\cos x\, dx", Magnification->3], BaseStyle->{Orange}], 
+                        BaseStyle->{FontSize->24}], 
+                Text[Style["Quarto Passo", 28, Orange, Bold]]
+                    -> Text["Ora, dalla tabella degli integrali fondamentali ricaviamo \nil risultato finale:\n" 
+                        Framed[MaTeX["3e^x+\\frac{2}{3}x^3-\\frac{1}{2}\\sin x + C", Magnification->3], BaseStyle->{Orange}], 
+                        BaseStyle->{FontSize->24}]
+            },
+            Appearance->Large
+        ],
+
+    WindowTitle->"Soluzione Guidata", WindowSize->All]
 
 Sol2 = PopupWindow[ 
     Button[Style["Vuoi vedere la soluzione?", FontSize->Large]],
-    TabView[{Text[Style[" Trucco >>> ", 36, Orange, Bold]] -> Text[Style["\n Seguire i passi per vedere la soluzione passo dopo passo...\n\n", 44]],
-    Text[Style["Primo Passo", 36, Orange, Bold]] -> Text["Utilizzando la proprieta' di linearita' vado a spezzare l'integrale in due pezzi:\n\n" Framed[MaTeX["\\int_{1}^{e} 3x^2+\\int_{1}^{e}-\\frac{2}{x} \, dx", Magnification->3], BaseStyle->{Orange}], BaseStyle->{FontSize->32}],
-    Text[Style["Secondo Passo", 36, Orange, Bold]] -> Text["Usiamo la II proprieta' di linearita' per portare fuori le varie costanti dagli integrali:\n\n" Framed[MaTeX["3\\int_{1}^{e} x^2 \, dx-2\\int_{1}^{e}\\frac{1}{x} \, dx", Magnification->3],BaseStyle->{Orange}], BaseStyle->{FontSize->32}],
-    Text[Style["Terzo Passo", 36, Orange, Bold]] -> Text["Risolviamo separatamente i due integrali fondamentali e quindi:\n\n" Framed[MaTeX["3\\int_{1}^{e} x^2 \,dx=3((\\frac{1}{3}e^3)-\\frac{1}{3}1^3) \\quad e \\quad -2\\int_{1}^{e} \\frac{1}{x} \,dx= -2\\log (\\frac{e}{1})", Magnification->3], BaseStyle->{Orange}], BaseStyle->{FontSize->32}], 
-    Text[Style["Quarto Passo", 36, Orange, Bold]] -> Text["Rimettendoli insieme avremo: \n\n" Framed[MaTeX["3((\\frac{1}{3}e^3)-\\frac{1}{3}1^3)-2\\log (\\frac{e}{1})=e^3-3", Magnification->3], BaseStyle->{Orange}], BaseStyle->{FontSize->32}]}, Appearance->Large],
-    WindowTitle->"Soluzione Guidata", WindowSize->All
-    ];
+
+    TabView[
+        {
+            (*Text[Style[" Trucco >>> ", 28, Orange, Bold]] 
+                -> Text[Style["\n Seguire i passi per vedere la soluzione passo dopo passo...\n", 24]],*)
+            Text[Style["Primo Passo", 28, Orange, Bold]] 
+                -> Text["Utilizzando la proprieta' di linearità vado a spezzare l'integrale in due pezzi:\n"
+                    Framed[MaTeX["\\int_{1}^{e} 3x^2+\\int_{1}^{e}-\\frac{2}{x} \, dx", Magnification->3], BaseStyle->{Orange}],
+                    BaseStyle->{FontSize->24}],
+            Text[Style["Secondo Passo", 28, Orange, Bold]]
+                -> Text["Usiamo la 2° proprieta' di linearità per portare fuori le varie costanti \ndagli integrali:\n" 
+                    Framed[MaTeX["3\\int_{1}^{e} x^2 \, dx-2\\int_{1}^{e}\\frac{1}{x} \, dx", Magnification->3],BaseStyle->{Orange}], 
+                    BaseStyle->{FontSize->24}],
+            Text[Style["Terzo Passo", 28, Orange, Bold]] 
+                -> Text["Risolviamo separatamente i due integrali fondamentali e quindi:\n" 
+                    Framed[MaTeX["3\\int_{1}^{e} x^2 \,dx=3((\\frac{1}{3}e^3)-\\frac{1}{3}1^3) \\quad e \\quad -2\\int_{1}^{e} \\frac{1}{x} \,dx= -2\\log (\\frac{e}{1})", Magnification->3], 
+                    BaseStyle->{Orange}], BaseStyle->{FontSize->24}], 
+            Text[Style["Quarto Passo", 28, Orange, Bold]] 
+                -> Text["Rimettendoli insieme avremo: \n" 
+                    Framed[MaTeX["3((\\frac{1}{3}e^3)-\\frac{1}{3}1^3)-2\\log (\\frac{e}{1})=e^3-3", Magnification->3], 
+                    BaseStyle->{Orange}], BaseStyle->{FontSize->24}]}, Appearance->Large],
+    
+    WindowTitle->"Soluzione Guidata", WindowSize->All];
