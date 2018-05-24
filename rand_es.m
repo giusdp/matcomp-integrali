e09681db12d1d55db106dc93a74eae559f7e4313
giusdp <depalma.gsp@gmail.com>
2018-05-24 17:01:59 +0200

    fix bottoni e aggiunti esercizi

--- rand_es.m
+++ rand_es.m
@@ -1,40 +1,43 @@
-<<"MaTeX`";
 <<"Soluzioni_guidate.m";
 
-(* il tipo di integrale è rappresentato da 1 e 2.
-    1 : integrale indefinito
-    2 : integrale definito
-*)
-esercizi = {
-    {"\\int \\sin{x} dx", "Usa questa cosa", "sin(x)", 1},
-    {"\\int \\cos{x} dx", "Usa quest'altra cosa", "cos(x)", 1},
-    {"\\int x^2", "Usa il trucco pippo", "x^2 from a to b", 2}
-};
+prendiACaso[esercizi_] := Module[{rand, es},
+    rand = RandomInteger[{1, Length[esercizi]}];
+    es = Extract[esercizi, rand];
+    Return[es]]
 
-prendiACaso[] := Module[{rand, es},
-rand = RandomInteger[{1, Length[esercizi]}];
-es = Extract[esercizi, rand];
-Return[es]]
-
-getList[] := Return[esercizi]
-len[] := Return[Length[esercizi]]
-
-buildSlide[] := For[i=0, i < 5,i++,Module[{esex, traccia, sugg, wolfram, tipo, sol}, 
-    esex = prendiACaso[];
+mostraEsercizi[esercizi_] := Module[{esex, traccia, sugg, wolfram, tipo, sol},
+    esex = prendiACaso[esercizi];
     traccia = Extract[esex, 1];
     sugg = Extract[esex, 2];
-    wolfram = Extract[esex, 3];
-    tipo = Extract[esex, Length[esex]];
-    sol := If[tipo == 1, indefinitiStepByStep[wolfram], definitiStepByStep[wolfram]];
-    Print[
-        Grid[
+    (*wolfram = Extract[esex, 3];*)
+    tipo = ToExpression[Extract[esex, Length[esex]]];
+    sol := If[tipo == 1, indefinitiStepByStep[traccia], definitiStepByStep[traccia]];
+    Return[
+        GridBox[
             {
                 {
-                    Text["Esercizio " <> ToString[i+1]], MaTeX[traccia, Magnification->2]},{
-                    PopupWindow[Button[Style["Mostra il suggerimento!", FontSize->Larger]], sugg]                    
-                    PopupWindow[Button[Style["Guarda la soluzione!", FontSize->Larger]], sol]
+                    MaTeX[ToString[traccia], Magnification->2]
+                },
+                {
+                    Framed[ Button[Style["Mostra il suggerimento!", FontSize->Larger], MessageDialog[sugg]] 
+                        PopupWindow[Button[Style["Vedi la soluzione guidata!", FontSize->Larger]], Pane[sol, ScrollBars->True], WindowTitle->"Soluzione!",
+                        WindowSize->{800, 440}, Editable->True],
+                        FrameStyle->Directive[Orange, 82], RoundingRadius->10, Alignment->Baseline]
                 }
-            }]
+            }, RowSpacings->5] // DisplayForm
         ];
     ]
+
+caricaEsercizi[] := Module[{exs},
+    exs = {};
+    f = OpenRead["test_es.txt"];
+    While[True, 
+        line = ReadLine[f];
+        If[line == EndOfFile, Break[]];
+        If[StringStartsQ[line, "%"], Continue[]];
+        entries = StringSplit[line, "&"];
+        AppendTo[exs, entries];
+    ]
+    Close[f];
+    Return[exs];
 ]
