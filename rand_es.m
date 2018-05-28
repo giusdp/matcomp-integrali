c7d1b88f66197642501635c62718907103119b65
giusdp <depalma.gsp@gmail.com>
2018-05-28 13:47:16 +0200

    esercizi misti finiti

--- rand_es.m
+++ rand_es.m
@@ -5,33 +5,30 @@ prendiACaso[esercizi_] := Module[{rand, es},
     es = Extract[esercizi, rand];
     Return[es]]
 
-mostraEsercizio[esercizi_] := Module[{esex, traccia, sugg, wolfram, tipo, sol},
-    esex = prendiACaso[esercizi];
-    traccia = Extract[esex, 1];
-    sugg = Extract[esex, 2];
-    (*wolfram = Extract[esex, 3];*)
-    tipo = ToExpression[Extract[esex, Length[esex]]];
-    sol := If[tipo == 1, stepIndefiniteFunction[traccia], stepDefiniteFunction[traccia]];
-    Return[
-        Grid[{
-                {MaTeX[ToString[traccia], Magnification->4],SpanFromLeft},
-                {
-                Framed[Tooltip[ButtonBar[{"Mostra il suggerimento!" :> MessageDialog["Suggerimento:\n"
-                    Framed[ToString[ToExpression[sugg]], BaseStyle-> 24, ImageSize->{315, 125}, FrameStyle->Red, 
-                    RoundingRadius->10, Alignment->Center]]}, ImageSize->{300, 100}, Alignment->Center, 
-                    BaseStyle->{Orange, 22, Italic, Bold, FontFamily->"Times"}], "Fammi vedere il suggerimento!"],
-                    Alignment->Center, RoundingRadius->5], 
-                "               ",
-                Framed[
-                    Tooltip[
-                        ButtonBar[
-                            {"Soluzione guidata" :> MessageDialog["Attendere il caricamento della soluzione."] sol},
-                            Method->"Queued", ImageSize->{300, 100}, Alignment->Center, BaseStyle->{Orange, 22, Italic, Bold, FontFamily->"Times"}
-                        ], "Fammi vedere i passi!"], 
-                    Alignment->Center, RoundingRadius->5
-                ]
+mostraEsercizio[esercizi_] := CreateDialog[
+    Module[{esex, traccia, wolfram, tipo, sol}, 
+        esex = prendiACaso[esercizi];
+        traccia = Extract[esex, 1];
+        sugg = Extract[esex, 2];
+        tipo = ToExpression[Extract[esex, Length[esex]]];
+        sol := If[tipo == 1, stepIndefiniteFunction[traccia], stepDefiniteFunction[traccia]];
+        Grid[
+            {
+                { MaTeX[ToString[traccia], Magnification -> 4], SpanFromLeft}, 
+                { Framed[ButtonBar[{"Mostra il suggerimento!" :> MessageDialog[
+                    Framed[sugg, BaseStyle -> 24, ImageSize -> {315, 125}, 
+                        FrameStyle -> Red, RoundingRadius -> 10, Alignment -> Center]]}, 
+                        ImageSize -> {300, 100}, Alignment -> Center, 
+                        BaseStyle -> {Orange, 22, Italic, Bold, FontFamily -> "Times"}]],
+                 "               ", 
+                 Framed[Tooltip[ButtonBar[{"Soluzione guidata" :> MessageDialog[
+                    "Attendere il caricamento della soluzione."] sol}, Method -> "Queued", 
+                    ImageSize -> {300, 100}, Alignment -> Center, 
+                    BaseStyle -> {Orange, 22, Italic, Bold, FontFamily -> "Times"}], "Fammi vedere i passi!"], 
+                    Alignment -> Center, RoundingRadius -> 5]
+                },{},
+                {DefaultButton["Close", DialogReturn[]], SpanFromLeft}
             }
-        }
         ]
     ]
 ]
@@ -49,3 +46,37 @@ caricaEsercizi[] := Module[{exs},
     Close[f];
     Return[exs];
 ]
+
+(* Funzione per testare il funzionamento degli esercizi 
+
+testEsercizi[esercizi_, indice_] := Module[{esex, traccia, tipo, sol},
+
+    esex = Extract[esercizi, indice];
+    traccia = Extract[esex, 1];
+    sugg = Extract[esex, 2];
+    tipo = ToExpression[Extract[esex, Length[esex]]];
+    sol := If[tipo == 1, stepIndefiniteFunction[traccia], stepDefiniteFunction[traccia]];
+    Print[
+        Grid[{
+                {esex,MaTeX[ToString[traccia], Magnification->4],SpanFromLeft},
+                {
+                Framed[Tooltip[ButtonBar[{"Mostra il suggerimento!" :> MessageDialog["Suggerimento:\n"
+                    Framed[Text[sugg], BaseStyle-> 24, ImageSize->{315, 125}, FrameStyle->Red, 
+                    RoundingRadius->10, Alignment->Center]]}, ImageSize->{300, 100}, Alignment->Center, 
+                    BaseStyle->{Orange, 22, Italic, Bold, FontFamily->"Times"}], "Fammi vedere il suggerimento!"],
+                    Alignment->Center, RoundingRadius->5], 
+                "               ",
+                Framed[
+                    Tooltip[
+                        ButtonBar[
+                            {"Soluzione guidata" :> MessageDialog["Attendere il caricamento della soluzione."] sol},
+                            Method->"Queued", ImageSize->{300, 100}, Alignment->Center, BaseStyle->{Orange, 22, Italic, Bold, FontFamily->"Times"}
+                        ], "Fammi vedere i passi!"], 
+                    Alignment->Center, RoundingRadius->5
+                ]
+            }
+        }
+        ]   
+]
+]
+*)
